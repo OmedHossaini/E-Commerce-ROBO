af5f128eb70f37a48b92738f37d0a2108b45a852
Ossama <otenessi@gmail.com>
2023-11-15 14:11:50 -0500

    batchImport.js

---
+++ server/BatchImport.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const fs = require('fs');
+
+const { MONGO_URI } = process.env;
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+// Read JSON files
+const itemsData = JSON.parse(fs.readFileSync('./data/items.json', 'utf8'));
+const companiesData = JSON.parse(fs.readFileSync('./data/companies.json', 'utf8'));
+
+const batchImport = async () => {
+    const client = new MongoClient(MONGO_URI, options);
+
+    try {
+        await client.connect();
+        const db = client.db("E-commerce");
+
+        // Import items data into Data_Items collection
+        const resultItems = await db.collection("Data_Items").insertMany(itemsData);
+        console.log(`Successfully inserted ${resultItems.insertedCount} items into Data_Items collection`);
+
+        // Import companies data into Data_Companies collection
+        const resultCompanies = await db.collection("Data_Companies").insertMany(companiesData);
+        console.log(`Successfully inserted ${resultCompanies.insertedCount} companies into Data_Companies collection`);
+    } catch (err) {
+        console.error("Error in importing data", err);
+    } finally {
+        await client.close();
+    }
+};
+
+batchImport();
